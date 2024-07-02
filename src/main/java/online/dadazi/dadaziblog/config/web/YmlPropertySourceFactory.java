cde427b4400b066d7b3da3c5edf5e55bb93ee5b5
liangqingnice <1083218658@qq.com>
2024-07-02 21:43:56 +0800

    增加Yml读取注释

--- src/main/java/online/dadazi/dadaziblog/config/web/YmlPropertySourceFactory.java
+++ src/main/java/online/dadazi/dadaziblog/config/web/YmlPropertySourceFactory.java
@@ -1,5 +1,8 @@
 package online.dadazi.dadaziblog.config.web;
 
+import cn.hutool.core.util.StrUtil;
+import lombok.extern.slf4j.Slf4j;
+import online.dadazi.dadaziblog.config.exception.CustomException;
 import org.springframework.boot.env.YamlPropertySourceLoader;
 import org.springframework.core.env.PropertySource;
 import org.springframework.core.io.support.EncodedResource;
@@ -10,8 +13,11 @@ import java.util.List;
 
 /**
  * 自定义Yml扫描
+ * 如果是多个yml需要@PropertySource注解 配置name属性来注入
+ *
  * @author lqk
  */
+@Slf4j
 public class YmlPropertySourceFactory implements PropertySourceFactory {
     @Override
     public PropertySource<?> createPropertySource(String name, EncodedResource resource) throws IOException {
